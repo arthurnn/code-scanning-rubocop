6bf07af5c01e578e72b36adafeaeaaf49be7dc96
Arthur Neves <arthurnn@gmail.com>
2020-05-01 14:52:14 -0400

    init gem

---
+++ lib/code/scanning/rubocop.rb
@@ -0,0 +1,10 @@
+require "code/scanning/rubocop/version"
+
+module Code
+  module Scanning
+    module Rubocop
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/code/scanning/rubocop/version.rb
@@ -0,0 +1,7 @@
+module Code
+  module Scanning
+    module Rubocop
+      VERSION = "0.1.0"
+    end
+  end
+end
