0b99ade1ec1d9cb5825d1ed15c0206fdacaf0c0e
Arthur Neves <arthurnn@gmail.com>
2020-05-05 13:03:09 -0400

    fix bundle install/add

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,8 @@ cd $GITHUB_WORKSPACE
 # Install correct bundler version
 gem install bundler -v "$(grep -A 1 "BUNDLED WITH" Gemfile.lock | tail -n 1)"
 
-bundle install
-
-bundle inject code-scanning-rubocop 0.2.0
+# bundle add will run install after injecting the gem
+bundle add code-scanning-rubocop --version 0.2.0
 
 bundle exec rubocop --require code_scanning --format CodeScanning::SarifFormatter -o rubocop.sarif
 
