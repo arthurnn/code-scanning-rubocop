cb6e1a58d81aa98a51735c34d7f42e044680ccf9
Arthur Neves <arthurnn@gmail.com>
2020-05-05 12:09:06 -0400

    Run code-scanning rubocop within bundler users setup

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,14 @@ set -x
 
 cd $GITHUB_WORKSPACE
 
-rubocop --require code_scanning --format CodeScanning::SarifFormatter -o rubocop.sarif
+# Install correct bundler version
+gem install bundler -v "$(grep -A 1 "BUNDLED WITH" Gemfile.lock | tail -n 1)"
+
+bundle install
+
+bundle inject code-scanning-rubocop "$(gem list | grep code-scanning-rubocop | tr -cd '0-9.')"
+
+bundle exec rubocop --require code_scanning --format CodeScanning::SarifFormatter -o rubocop.sarif
 
 if [ ! -f rubocop.sarif ]; then
     exit 1
