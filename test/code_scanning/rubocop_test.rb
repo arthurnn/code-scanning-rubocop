2939e7a1b2866dc3bc7a443e8b9b6b2de91eadb8
Arthur Neves <arthurnn@gmail.com>
2020-05-06 09:49:04 -0400

    baseline for tests

---
+++ test/code_scanning/rubocop_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class CodeScanning::RubocopTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::CodeScanning::Rubocop::VERSION
+  end
+
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,4 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
-require "code/scanning/rubocop"
+require "code_scanning"
 
 require "minitest/autorun"
