0f31f37f2401757c5a630f462daec3e983bb8af3
Arthur Neves <arthurnn@gmail.com>
2020-05-06 09:51:25 -0400

    fmt

--- test/code_scanning/rubocop_test.rb
+++ test/code_scanning/rubocop_test.rb
@@ -1,8 +1,9 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class CodeScanning::RubocopTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::CodeScanning::Rubocop::VERSION
   end
-
 end
